d536c6e58963eaf00d7b19dacc8567e598a2df6d
Jack Cooper <Jack_cooper01@yahoo.com>
2023-11-28 21:39:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -36,6 +36,28 @@ document.getElementById('navToggle').addEventListener('click', function() {
     }
 });
 
+// Lazy Loading for performance
+document.addEventListener('DOMContentLoaded', function() {
+    const lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
+
+    if ("IntersectionObserver" in window) {
+        let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    let lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.src;
+                    lazyImage.classList.remove('lazy');
+                    lazyImageObserver.unobserve(lazyImage);
+                }
+            });
+        });
+
+        lazyImages.forEach(function(lazyImage) {
+            lazyImageObserver.observe(lazyImage);
+        });
+    }
+});
+
 // Hover Effects
 document.querySelectorAll('.hover-effect').forEach(item => {
     item.addEventListener('mouseover', () => {
